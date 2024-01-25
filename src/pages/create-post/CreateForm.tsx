a4ddf0e3f5230a11971b941c7fa54cfaf4ecfd99
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-01-25 19:42:56 +0100

    Navigate to home page after creating post

--- src/pages/create-post/CreateForm.tsx
+++ src/pages/create-post/CreateForm.tsx
@@ -4,6 +4,7 @@ import { yupResolver} from "@hookform/resolvers/yup"
 import { addDoc, collection } from "firebase/firestore"
 import { auth, db } from "../../config/firebase"
 import { useAuthState } from "react-firebase-hooks/auth"
+import { useNavigate } from "react-router-dom"
 
 interface ICreatePostForm {
   title: string;
@@ -12,6 +13,8 @@ interface ICreatePostForm {
 
 export const CreateForm = () => {
   const [user] = useAuthState(auth);
+
+  const navigate = useNavigate();
 
   const schema = yup.object().shape({
     title: yup.string().required("You must add a title"),
@@ -30,6 +33,7 @@ export const CreateForm = () => {
       username: user?.displayName,
       user_id: user?.uid
     })
+    navigate('/');
   }
 
   return (
