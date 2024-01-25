c5feaedefda9a9a5ad059a77bfaa8f3fa6f9ae4e
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2024-01-25 18:30:05 +0100

    Add styles to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,14 +11,16 @@ export const Navbar = () => {
   }
 
   return (
-    <div>
-      <Link to={'/'}>Main</Link>
-      <Link to={'/login'}>Login</Link>
+    <div className="navbar">
+      <div className="links">
+        <Link to={'/'}>Main</Link>
+        {!user && <Link to={'/login'}>Login</Link>}
+      </div>
 
       {user && (
-      <div>
+      <div className="user">
         <p>{user?.displayName}</p>
-        <img src={user?.photoURL || ""} width="100" height="100"/>
+        <img src={user?.photoURL || ""} width="50" height="50"/>
         <button onClick={logOut}>Log out</button>
       </div>
       )}
